8aa31db875163849d4539cbda233dc2c86f17589
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-07-29 09:57:55 +0200

    feat(hero): structure and files  + styling

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+import phoneHand from '../img/phone_hand.png';
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContainer}>
+        <article className={styles.heroArticle}>
+          <h2>Poznaj Lorem Ipsum!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo porro ullam at
+            perferendis aliquid voluptates suscipit quo odio! At veritatis amet repellat
+            quod soluta tempore expedita reiciendis ducimus quae nisi. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit
+          </p>
+          <button className={styles.btnOrange}>O aplikacji</button>
+          <button>Pobierz</button>
+        </article>
+        <img className={styles.heroImage} src={phoneHand} alt="phone-hand" />
+      </div>
+    </section>
+  );
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1>Home</h1>
+      <Hero />
     </main>
   );
 }
