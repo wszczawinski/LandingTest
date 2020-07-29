45d890ff8a2890a30c8b682c844e81d406952cca
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-07-29 14:51:27 +0200

    feat(navbar): background onscroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
+  let [scrollClass, setScrollClass] = useState(false);
+
+  window.addEventListener('scroll', () => {
+    let scroll = true;
+    if (window.scrollY === 0) {
+      scroll = false;
+    }
+    setScrollClass(scroll);
+  });
+
   return (
-    <nav className={styles.navbar}>
-      <Link to="/" className={styles.navbarLogo}>
-        Logo
-      </Link>
-      <ul className={styles.navbarList}>
-        <li>Start</li>
-        <li>Jak to działa?</li>
-        <li>Cennik</li>
-        <li>Logowanie</li>
-        <li>Rejestracja</li>
-      </ul>
+    <nav className={` ${styles.navbar} ${scrollClass ? styles.transparent : null} `}>
+      <div className={styles.navbarContainer}>
+        <Link to="/" className={styles.navbarLogo}>
+          Logo
+        </Link>
+        <ul className={styles.navbarList}>
+          <li>Start</li>
+          <li>Jak to działa?</li>
+          <li>Cennik</li>
+          <li>Logowanie</li>
+          <li>Rejestracja</li>
+        </ul>
+      </div>
     </nav>
   );
 }
